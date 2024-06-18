5b856b0475003b9b8ebc4325c4e1253350944fab
SONUshilla <127027721+SONUshilla@users.noreply.github.com>
2024-06-18 17:05:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -60,6 +60,7 @@ app.use(session({
   cookie: {
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days in milliseconds
     secure: false, // Set to true if using HTTPS
+    httpOnly:false,
   }
 }));
 console.log("for commit");
